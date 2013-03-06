04ea9e462d8c8ddb00db020ff84f3076ec40623c
Fredrik Olsson <fr3gu77@gmail.com>
2013-03-06 16:20:40 +0100

    Generic Mod tutorial

---
+++ common/tutorial/generic/CommonProxy.java
@@ -0,0 +1,11 @@
+package tutorial.generic;
+
+public class CommonProxy {
+        public static String ITEMS_PNG = "/items.png";
+        public static String BLOCK_PNG = "/block.png";
+        
+        // Client stuff
+        public void registerRenderers() {
+                // Nothing here as the server doesn't render graphics!
+        }
+}
